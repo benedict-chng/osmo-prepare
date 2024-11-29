f726760ca2d3a2424cd5d2b402ce3d647ff344f3
Benedict Chng <benedict_chng@yahoo.com>
2024-11-30 10:33:48 +1100

    Added join.py for joining multiple MP4 files

---
+++ join.py
@@ -0,0 +1,30 @@
+import sys
+from main import create_input_parameter_file, join_videofile
+
+
+def get_filename_list() -> list[str]:
+    filenames: list[str] = []
+    for i, filename in enumerate(sys.argv[2:]):
+        filenames.append(filename)
+    return filenames
+
+
+if __name__ == '__main__':
+
+    if (len(sys.argv) < 3):
+        print('Join 2 or more video files together')
+        print('Usage:')
+        print('    python join.py [dest] [file1] [file2] ...')
+        sys.exit(1)
+
+    processed_dir = './processed'
+
+    filename_list = get_filename_list()
+    output_filename = sys.argv[1]
+
+    create_input_parameter_file(processed_dir=processed_dir, filenames=filename_list)
+
+    join_videofile(processed_dir=processed_dir, output_filename=output_filename)
+
+    print('All done!\n')
+
